a503acd4404094c0b9f4efc7be9060217b9b7d19
K. <k@example.com>
2026-03-10 17:00:16 -0500

    Added PS code that now makes the test 401 out I wonder why

--- src/pshelperfuncapp/SayHello/run.ps1
+++ src/pshelperfuncapp/SayHello/run.ps1
@@ -2,7 +2,27 @@ using namespace System.Net
 
 param($Request, $TriggerMetadata)
 
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body       = "Hello, world!"
-})
+# Short-circuit response content if login not making it to backend
+if (-not $adoToken) {
+    Push-OutputBinding `
+        -Name 'Response' `
+        -Value ( `
+            [HttpResponseContext]@{
+            StatusCode  = [HttpStatusCode]::Unauthorized
+            ContentType = "text/plain"
+            Body        = "No EasyAuth access token found. Are you signed in?"
+        } `
+    )
+    return
+}
+
+# Say hello if we made it this far
+Push-OutputBinding `
+    -Name 'Response' `
+    -Value ( `
+        [HttpResponseContext]@{
+        StatusCode  = [HttpStatusCode]::OK
+        ContentType = "text/plain"
+        Body        = "Hello, world!"
+    } `
+)
